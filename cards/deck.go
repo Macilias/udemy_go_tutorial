3b103f850e3531e6bd9e53552cfbffa8769fdaf8
Maciek Niemczyk <Maciek.Niemczyk@vitraum.de>
2017-11-21 10:04:57 +0100

    init 1 save/read

--- cards/deck.go
+++ cards/deck.go
@@ -1,4 +1,11 @@
-package cards
+package main
+
+import (
+	"fmt"
+	"strings"
+	"io/ioutil"
+	"os"
+)
 
 type deck []string
 
@@ -15,4 +22,32 @@ func newDeck() deck {
 	}
 
 	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ", ")
+}
+
+func (d deck) saveToFile(fileName string) error {
+	return ioutil.WriteFile(fileName, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(fileName string) deck {
+	bs, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	split := strings.Split(string(bs), ", ")
+	return deck(split)
 }
