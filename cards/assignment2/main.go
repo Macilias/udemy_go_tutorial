57095e6fbef8fdd82bf8525cd14c5774fcb910ed
Maciek Niemczyk <Maciek.Niemczyk@vitraum.de>
2017-11-22 08:49:06 +0100

    init 2 assignemt2

--- cards/assignment2/main.go
+++ cards/assignment2/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type shape interface {
 	getArea() float64
 }
@@ -8,5 +10,19 @@ type triangle struct{}
 type square struct{}
 
 func main() {
+	t, s := triangle{}, square{}
+	printArea(t)
+	printArea(s)
+}
 
+func printArea(s shape) {
+	fmt.Println(s.getArea())
+}
+
+func (triangle) getArea() float64 {
+	return 3
+}
+
+func (square) getArea() float64 {
+	return 2
 }
