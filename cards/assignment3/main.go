42638bea94e716de33fb84efe4b184918e00e352
Maciek Niemczyk <Maciek.Niemczyk@vitraum.de>
2017-11-22 13:11:44 +0100

    init 2 assignemt3 + init channels

---
+++ cards/assignment3/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+
+	if len(os.Args) < 2 {
+		log.Fatal("no file name provided?")
+	}
+
+	filename := os.Args[1]
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	io.Copy(os.Stdout, f)
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
